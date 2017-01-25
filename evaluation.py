0c00f2c06358380d90d2758bf684d744dbbfb8ac
miguelemosreverte <miguellemos95@gmail.com>
2017-01-25 11:07:59 -0300

    Added the last changes made to evaluation.py from the master branch, which for now is the branch where the GTK version is being delevoped.

--- evaluation.py
+++ evaluation.py
@@ -42,42 +42,49 @@ def creation_date(path_to_file):
             # so we'll settle for when its content was last modified.
             return stat.st_mtime
 
-def filterTER (line):
-    if "Total TER:" in line:
-        line = line.replace("Total TER:", "")
-        line = line.split('(', 1)[0]
-        line += "\n"
-        return line
-    else:
-        return ''
+def filterTER (lines):
+    result = ''
+    warning = False
+    lines = lines.splitlines()
+    for line in lines:
+        if "Total TER:" in line:
+            result += line.replace("Total TER:","")
+        if "Warning, Invalid line:" in line:
+            warning = True
+    if warning:
+        result += " There are lines unchanged from source to reference. HTER cannot work in those cases."
+    return result + "\n"
 
 def filterBLEU (line, BLEU_type):
     if BLEU_type == "BLEU":      line = line.split(',', 1)[0]
     if BLEU_type == "BLEU2GRAM": line = line.split(',', 1)[1].split('/')[0]
     if BLEU_type == "BLEU3GRAM": line = line.split(',', 1)[1].split('/')[1]
     if BLEU_type == "BLEU4GRAM": line = line.split(',', 1)[1].split('/')[2]
+    line = line.replace('\n','').replace('\r', '')
     return line
 
-def filter_verbose_output(output,method):
-    specific_line = ""
-    while True:
-      line = output.stdout.readline()
-      if line != '':
-        if method == "TER": filtered_line = filterTER(line)
-        #add more complicated methods to filter over here
+def filterGTM (line):
+    if "You should not be comparing equal runs" in line:
+        line = "There are lines unchanged from source to reference. GTM cannot work in those cases.\n"
+    return line
 
-        if filtered_line != '':
-            specific_line = filtered_line
-      else:
-        break
-    return specific_line
+def filter_output(proccess,method):
+    out, err = proccess.communicate()
+    final_text = ""
+    if not err :
+        final_text = out
+    else: final_text = err
+    if method == "TER": final_text = filterTER(final_text)
+    if method == "GTM": final_text = filterGTM(final_text)
+
+    return final_text
 
 cached_results = {}
 
 def evaluate(checkbox_indexes, test, reference):
     checkbox_indexes_constants = ["WER","PER","HTER", "GTM", "BLEU","BLEU2GRAM","BLEU3GRAM","BLEU4GRAM"]
     DIRECTORY = os.path.abspath("evaluation_scripts") + "/"
-    TER_DIRECTORY = DIRECTORY + "tercom-0.7.25/src/"
+    TER_DIRECTORY = DIRECTORY + "tercom-0.7.25/tercom.7.25.jar"
     GTM_DIRECTORY = DIRECTORY + "gtm-1.4/"
     EXEC_PERL = "perl "
     EXEC_JAVA = "java "
@@ -91,31 +98,32 @@ def evaluate(checkbox_indexes, test, reference):
             else:
                 if checkbox_indexes_constants[checkbox_index] == "WER" or checkbox_indexes_constants[checkbox_index] == "PER":
                     command = EXEC_PERL + DIRECTORY +  checkbox_indexes_constants[checkbox_index] + ".pl" + " -t " + test + " -r " + reference
-                    print command
                     proc = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE)
                     result = "\n" + checkbox_indexes_constants[checkbox_index] + "..... " + proc.stdout.read()
                     return_results += result
                     cached_results[key] =  result
 
-
                 if checkbox_indexes_constants[checkbox_index] == "HTER":
-                    command_2 = EXEC_JAVA + "-cp " + TER_DIRECTORY + " TERtest " + " -r " + reference + " -h " + test
-                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE)
-                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_verbose_output(proc,"TER")
+                    command_2 = EXEC_JAVA + "-jar " + TER_DIRECTORY + " -r " + reference + " -h " + test
+                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_output(proc,"TER")
                     return_results += result
                     cached_results[key] =  result
 
 
                 if checkbox_indexes_constants[checkbox_index] == "GTM":
                     command_2 = EXEC_JAVA + "-cp " + GTM_DIRECTORY + " gtm" + " -t " +  test + " " + reference
-                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE)
-                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_verbose_output(proc,"GTM")
+                    proc = subprocess.Popen(command_2, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + filter_output(proc,"GTM")
+
+                    #out, err = proc.communicate()
+                    #result = "\n" + checkbox_indexes_constants[checkbox_index] + "....." + out + err
                     return_results += result
                     cached_results[key] =  result
 
                 if "BLEU" in checkbox_indexes_constants[checkbox_index] and BLEU_cached_results == "":
-                    command = EXEC_PERL + DIRECTORY + "BLEU.pl " + test +" < " + reference
-                    proc = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE)
+                    command = EXEC_PERL + DIRECTORY + "BLEU.pl " + reference +" < " + test
+                    proc = subprocess.Popen(command, shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     while True:
                       line = proc.stdout.readline()
                       if line != '':BLEU_cached_results += line
@@ -129,4 +137,5 @@ def evaluate(checkbox_indexes, test, reference):
 
 
         checkbox_index += 1
+    return_results
     return return_results
